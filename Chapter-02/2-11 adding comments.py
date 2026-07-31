7895f53a5153ffc2a392c76ba1dab95b4acdb94d
Fatima Jafarli <fatimacafarli9@gmail.com>
2026-07-31 17:54:51 +0400

    complete chapter 02

---
+++ Chapter-02/2-11 adding comments.py
@@ -0,0 +1,15 @@
+#Fatya
+#July 31, 2026
+#This program combines the nameof the famous person and their quote.
+famous_person = "Albert Enstein"
+quote = "A person who never made a mistake never tried anything new"
+message= f'{famous_person} once said, "{quote}"'
+print(message)
+
+#Fatya
+#July 21, 2026
+#This program prints the number 8 using different mathematical operations.
+print(4+4)
+print(15-7)
+print(2*4)
+print(16/2)
